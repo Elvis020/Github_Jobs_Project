f779db130eb13333c829d5b7f8656ffbbd1ad53e
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-11-14 09:52:24 +0000

    Added Search form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import useFetchJobs from './useFetchJobs';
 import {Container} from 'react-bootstrap';
 import Job from './Job';
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
 
-  const {jobs,error,loading} = useFetchJobs(params, page);
+
+  // Function to handle Input Change ehen the user searches for jobs
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1)
+    setParams(prevParams => {
+      return {...prevParams, [param]: value}
+    })
+  }
+
+  const {jobs,error,loading, hasNextPage} = useFetchJobs(params, page);
   return (
     <Container className="my-4">
     <h1 className="mb-4 text-center text-white">Github Jobs</h1>
-    <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+    <SearchForm params={params} onParamsChange={handleParamChange} />
+    
+    <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error occured. Try refreshing the page</h1>}
       {/* {jobs && <h1>{jobs.length}</h1>} */}
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,54 +1,68 @@
-import {useReducer, useEffect} from 'react';
-import Axios from 'axios';
+import { useReducer, useEffect } from "react";
+import Axios from "axios";
 
 // Seting up types to help spot errors
-const ACTIONS ={
-    MAKE_REQUEST: 'make_request',
-    GET_DATA: 'get-data',
-    ERROR: 'error'
-}
-
-
+const ACTIONS = {
+  MAKE_REQUEST: "make_request",
+  GET_DATA: "get-data",
+  ERROR: "error",
+  UPDATE_NEXT_PAGE: "update-next-page",
+};
 
 // Setting up reducer function
 const reducer = (state, action) => {
-    switch (action.type) {
-      case ACTIONS.MAKE_REQUEST:
-          return {loading: true, jobs:[]}
-      case ACTIONS.GET_DATA:
-          return {...state, loading: false, jobs: action.payload.jobs}
-      case ACTIONS.ERROR:
-          return {...state, error: true, jobs: action.payload.error}
-      default:
-        return state;
-    }
-}
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return { ...state, error: true, jobs: action.payload.error };
+    case ACTIONS.UPDATE_NEXT_PAGE:
+      return { ...state, hasNextPage: action.payload.hasNextPage };
+    default:
+      return state;
+  }
+};
 
 // API for fetching github jobs and adding cors anywhere to help us with cors
-// const apiUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
 const apiUrl = "https://api.allorigins.win/raw?url=https://jobs.github.com/positions.json";
-//localhost:8010/proxy
-// const apiUrl = "http://locahost:8010/proxy/positions.json";
 
-export default function useFetchJobs(params, page){
-    const [state,dispatch]  = useReducer(reducer,{jobs: [], loading: true})
+export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
-    useEffect(() => {
-        const cancelToken = Axios.CancelToken.source(); //Being able to cancel the token request
-        dispatch({ type: ACTIONS.MAKE_REQUEST });
-        Axios.get(apiUrl, {
-            cancelToken: cancelToken.token, 
-            params:{...params, page:page,markdown:true}})
-            .then(res => {
-                dispatch({type: ACTIONS.GET_DATA, payload:{jobs: res.data}})
-            })
-            .catch((e) => {
-                if(Axios.isCancel(e)) return // If Axios request is cancelled it should return a silent error
-                dispatch({type: ACTIONS.ERROR, payload:{error: e}})
-            })
-        return () => {
-            cancelToken.cancel();
-        }
-    }, [params,page])
-    return state;
+  useEffect(() => {
+    const cancelToken1 = Axios.CancelToken.source(); //Being able to cancel the token request
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    Axios.get(apiUrl, {
+      cancelToken: cancelToken1.token,
+      params: { ...params, page: page, markdown: true },
+    })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        if (Axios.isCancel(e)) return; // If Axios request is cancelled it should return a silent error
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+
+    const cancelToken2 = Axios.CancelToken.source(); //Being able to cancel the token request
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    Axios.get(apiUrl, {
+      cancelToken: cancelToken2.token,
+      params: { ...params, page: page + 1, markdown: true },
+    })
+      .then((res) => {
+        dispatch({ type: ACTIONS.UPDATE_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } });
+      })
+      .catch((e) => {
+        if (Axios.isCancel(e)) return; // If Axios request is cancelled it should return a silent error
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+    return () => {
+      cancelToken1.cancel();
+      cancelToken2.cancel();
+    };
+  }, [params, page]);
+  return state;
 }
